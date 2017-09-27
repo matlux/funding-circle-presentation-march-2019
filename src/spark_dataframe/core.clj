45a9236b6f3939320c5a00643b0fe390ba28d1b4
Mathieu Gauthron <mathieu@matlux.net>
2017-09-27 18:22:55 +0100

    added columns for year and month

--- src/spark_dataframe/core.clj
+++ src/spark_dataframe/core.clj
@@ -1,5 +1,9 @@
 (ns spark-dataframe.core
-  (:import (org.apache.spark.sql SparkSession)))
+  (:import (org.apache.spark.sql SparkSession)
+           (org.apache.spark.sql Column)
+           (org.apache.spark.sql functions)
+    ;;(org.apache.spark.sql.functions$ MODULE$)
+           ))
 
 
 
@@ -7,6 +11,40 @@
                (appName "Simple app")
                (master "local[*]")
                getOrCreate))
+(def  opts {"header" "true",
+                     "timestampFormat" "dd/MM/yyyy",
+            "inferSchema" "true"})
+
+(def df (.. spark
+            read
+            (options opts)
+            (csv "/Users/mathieu/Dropbox/finance/investment-transactions/Matlux_rate-setter_LenderTransactions_all_2017-07-31.csv")))
+
+(def df2 (.. df
+             (withColumn "month" (functions/month (new Column "date")))
+             (withColumn "year" (functions/year (new Column "date")))))
+
+
+(comment
+  (new org.apache.spark.sql.Column "date")
+  (new Column "date")
+
+
+
+  df
+
+  ;;org.apache.spark.sql.Column$/MODULE$
+
+  ;;(new org.apache.spark.sql.functions$/MODULE)
+  (functions/month (new Column "date"))
+
+  ;;(. month (. month org.apache.spark.sql.functions$/MODULE$ (new Column "date")))
+
+
+
+  (.show df2)
+  )
+
 
 (defn foo
   "I don't do a whole lot."
@@ -14,3 +52,6 @@
   (println x "Hello, World!" spark))
 
 
+
+(defn -main [& args]
+  spark)
